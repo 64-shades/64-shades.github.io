b323ee4df95880620cc9faee8804461325dc3752
John Bampton <jbampton@gmail.com>
2025-09-05 03:00:26 +1000

    Add files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_site/**/*.{html}'
+  ],
+  plugins: [],
+  safelist: [
+    'role-tal',
+    'role-fischer',
+    'role-kasparov'
+  ],
+  theme: {},
+};
