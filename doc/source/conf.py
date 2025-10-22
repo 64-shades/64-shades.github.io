b7076859bd29bf8cbbc29262c354055054b771ef
Subham Sangwan <subhamsangwan26@gmail.com>
2025-10-22 15:15:37 +0530

    Set up html_favicon for custom favicon image in Sphinx docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,12 +3,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "64 Shades"
 copyright = "2025, 64 Shades"
 author = "64 Shades"
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,6 +23,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = []  # type: ignore
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -28,6 +31,9 @@ html_theme = "alabaster"
 html_static_path = ["_static"]
 
 html_logo = "_static/images/logo.jpeg"
+
+# Favicon for the documentation site
+html_favicon = "_static/images/favicon.ico"
 
 # Add your custom CSS file (updated to use the new CSS variables)
 html_css_files = ["css/custom.css"]
